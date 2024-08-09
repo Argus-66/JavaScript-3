7e4468572ecf92c8047ca180aa19e55897f07ea7
Ayush H <jqghebri@gmail.com>
2024-08-09 18:18:22 +0530

    creation

--- tut2_Promises.js
+++ tut2_Promises.js
@@ -3,9 +3,16 @@ const PormiseOne = new Promise(function(res,req) {
     //Do an async task
     setTimeout(function() {
         console.log('Async task completed');
+        res()
     }, 1000);
 });
 
-PormiseOne.then(function() {
-    console.log('Promise One resolved');
+new Promise(function(res,req) {
+    setTimeout(function(){
+        console.log("Async task 2 completed");
+        res();
+        
+    }, 1000);
+}).then(function() {
+    console.log("Promise 2 resolved");
 })
