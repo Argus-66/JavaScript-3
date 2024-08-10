5768649ce1fd4b5e0793cfc4d22d6ddd54383774
Ayush H <jqghebri@gmail.com>
2024-08-11 03:40:21 +0530

    creation

--- tut11.js
+++ tut11.js
@@ -7,7 +7,26 @@ console.log(descripter);
 const chai = {
     name: "Ginger chai",
     price: 250,
-    isAvailable: true
+    isAvailable: true,
+
+    orderChai: function(){
+        console.log("Code fat gya")
+    }
 }
 
-console.log(chai)
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+Object.defineProperty(chai, "name", {
+    //writable: false,
+    enumerable: false
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+
+for (let [key, value] of Object.entries(chai)) {
+    if(typeof value !== "function"){
+        console.log(`${key}: ${value}`);
+    }
+    
+}
