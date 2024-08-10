d3b8bf9dd4260e40f71c9623a71a1628192d3dc1
Ayush H <jqghebri@gmail.com>
2024-08-10 15:35:12 +0530

    creation

--- tut3.js
+++ tut3.js
@@ -1,7 +1,27 @@
 const user = {
     username: 'John Doe',
     loginCount: 100,
-    signedIn: true
+    signedIn: true,
+
+    getUserDetails:  function() {
+        console.log(`Username: ${this.username}`)
+        console.log(this)
+    }
 }
 
 console.log(user.username)
+console.log(user.getUserDetails())
+
+
+
+function User(username, loginCount, isLoggedIn) {
+    this.username = username
+    this.loginCount = loginCount
+    this.signedIn = isLoggedIn
+
+    return this
+}
+
+const UserOne = User('Jane Doe', 50, false)
+const UserTwo = User('Jack Doe', 100, true) //Overwrites the properties of UserOne
+console.log(UserOne)
