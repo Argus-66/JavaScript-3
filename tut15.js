ab421df762b185fbc1854924c721d3c76563e9a6
Ayush H <jqghebri@gmail.com>
2024-08-11 16:07:17 +0530

    creation

--- tut15.js
+++ tut15.js
@@ -39,3 +39,22 @@ function makeFunc(){
 
 const myFunc = makeFunc();
 myFunc();
+
+
+//Changing color of button using closure
+
+// document.getElementById("orange").onclick =function(){
+//     document.body.style.backgroundColor = `orange`;
+// }
+
+
+function clickHandler(color){
+    //document.body.style.backgroundColor = `${color}}`
+
+    return function (){
+        document.body.style.backgroundColor = `${color}`;
+    }
+}
+
+document.getElementById("orange").onclick = clickHandler("orange")
+document.getElementById("green").onclick = clickHandler("green")
